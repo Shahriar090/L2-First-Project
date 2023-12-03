769de05372af0863c37b198cd3d9cc16181d4ab3
Shahriar090 <shahriarh655@gmail.com>
2023-12-03 18:52:43 +0600

    Fix some issues from interface model and schema

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,11 +1,45 @@
+// import { Schema, model } from 'mongoose';
+// import { TUser } from './user.interface';
+
 import { Schema, model } from 'mongoose';
 import { TUser } from './user.interface';
 
-export const userSchema = new Schema<TUser>(
+// export const userSchema = new Schema<TUser>(
+//   {
+//     id: { type: String, required: true },
+//     password: { type: String, required: true },
+//     needsPasswordChange: { type: Boolean, default: true },
+//     role: {
+//       type: String,
+//       enum: ['student', 'faculty', 'admin'],
+//     },
+//     status: {
+//       type: String,
+//       enum: ['in-progress', 'blocked'],
+//       default: 'in-progress',
+//     },
+//     isDeleted: { type: Boolean, default: false },
+//   },
+//   { timestamps: true },
+// );
+
+// // model
+// export const User = model<TUser>('User', userSchema);
+
+const userSchema = new Schema<TUser>(
   {
-    id: { type: String, required: true },
-    password: { type: String, required: true },
-    needsPasswordChange: { type: Boolean, default: true },
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
     role: {
       type: String,
       enum: ['student', 'faculty', 'admin'],
@@ -15,10 +49,12 @@ export const userSchema = new Schema<TUser>(
       enum: ['in-progress', 'blocked'],
       default: 'in-progress',
     },
-    isDeleted: { type: Boolean, default: false },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
 
 // model
+
 export const User = model<TUser>('User', userSchema);
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,10 +1,16 @@
 import { z } from 'zod';
+
 const userValidationSchema = z.object({
+  id: z.string(),
   password: z
-    .string({ invalid_type_error: 'Password must be string' })
-    .max(20, { message: 'Password cannot be more than 20 characters' })
-    .optional(),
+    .string()
+    .min(20, { message: 'Password cannot be more than 20 characters' }),
+  needsPasswordChange: z.boolean().optional().default(true),
+  role: z.enum(['student', 'faculty', 'admin']),
+  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
+  isDeleted: z.boolean().optional().default(false),
 });
-export const userValidation = {
+
+export const UserValidation = {
   userValidationSchema,
 };
