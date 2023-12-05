fdfcccbbf41eace5e6955efdc613d2432a3b70c1
Shahriar090 <shahriarh655@gmail.com>
2023-12-05 12:53:38 +0600

    Organized the routing system

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 // parser
@@ -11,14 +10,13 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
-const getAController = (req: Request, res: Response) => {
+app.use('/api/v1', router);
+const test = (req: Request, res: Response) => {
   const a = 'welcome to the server of down fall';
   res.send(a);
 };
 
-app.get('/', getAController);
+app.get('/', test);
 
 // global error handler function
 app.use(globalErrorHandler);
